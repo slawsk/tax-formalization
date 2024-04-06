76b568d5b8c97a743fd63e173f7f142d0e5cc969
Sarah Lawsky <sarahlawsky@gmail.com>
2024-04-06 08:20:08 -0500

    Add files via upload

---
+++ capgainspicswebsite.py
@@ -0,0 +1,41 @@
+image_filename_01 = 'fig_01_excess_of.png'
+encoded_image_01 = base64.b64encode(open(image_filename_01, 'rb').read())
+
+image_filename_02 = 'fig_02_excess_of_A_B.png'
+encoded_image_02 = base64.b64encode(open(image_filename_02, 'rb').read())
+
+image_filename_03 = 'fig_03_net_cap_loss.png'
+encoded_image_03 = base64.b64encode(open(image_filename_03, 'rb').read())
+
+image_filename_04 = 'fig_04_net_cap_loss_carryforward.png'
+encoded_image_04 = base64.b64encode(open(image_filename_04, 'rb').read())
+
+image_filename_05 = 'fig_05_low_income.png'
+encoded_image_05 = base64.b64encode(open(image_filename_05, 'rb').read())
+
+image_filename_06 = 'fig_06_low_income_with_dot.png'
+encoded_image_06 = base64.b64encode(open(image_filename_06, 'rb').read())
+
+image_filename_07 = 'fig_07_cliff.png'
+encoded_image_07 = base64.b64encode(open(image_filename_07, 'rb').read())
+
+image_filename_08 = 'fig_08_low_income_correct_carryforward.png'
+encoded_image_08 = base64.b64encode(open(image_filename_08, 'rb').read())
+
+image_filename_09 = 'fig_09_all_frame.png'
+encoded_image_09 = base64.b64encode(open(image_filename_09, 'rb').read())
+
+image_filename_10 = 'fig_10_lt_fill.png'
+encoded_image_10 = base64.b64encode(open(image_filename_10, 'rb').read())
+
+image_filename_11 = 'fig_11_st_fill.png'
+encoded_image_11 = base64.b64encode(open(image_filename_11, 'rb').read())
+
+image_filename_12 = 'fig_12_hash_labels.png'
+encoded_image_12 = base64.b64encode(open(image_filename_12, 'rb').read())
+
+image_filename_13 = 'fig_13_dividing_line.png'
+encoded_image_13 = base64.b64encode(open(image_filename_13, 'rb').read())
+
+image_filename_14 = 'fig_14_dividing_line_and_OI.png'
+encoded_image_14 = base64.b64encode(open(image_filename_14, 'rb').read())
